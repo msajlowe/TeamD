1a71b5b51259e128b682e8ec8f9bb0f5c9ba6913
msajlowe <43071930+msajlowe@users.noreply.github.com>
2020-04-30 16:36:02 -0500

    Add files via upload

--- Project_P2_Team_D/ProjectP2TeamD/ProjectP2TeamD/ViewController.swift
+++ Project_P2_Team_D/ProjectP2TeamD/ProjectP2TeamD/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func salaryText(_ sender: Any) {
+    }
+    @IBAction func minRide(_ sender: Any) {
+    }
+    @IBAction func maxRide(_ sender: Any) {
+    }
+    
+    @IBAction func findRate(_ sender: Any) {
+        
+    }
+    
+    @IBAction func rateYourDriver(_ sender: Any) {
+        
+    }
+    
+    
 }
 
